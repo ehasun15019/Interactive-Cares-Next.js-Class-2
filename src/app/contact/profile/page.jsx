26a3f4c1fcbed2ca10eb34579c6c607679065bb3
Your Full Name <ehasun0909@gamil.com>
2025-07-16 21:49:26 +0600

    loading ta kaj korteche ki na ata bojar jonno profile folder a page.jsx a 2 sec delay kore disi to it's working fine

--- src/app/contact/profile/page.jsx
+++ src/app/contact/profile/page.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 
-const page = () => {
+const delay = (ms) =>
+  new Promise((resolve) => {
+    return setTimeout(resolve, ms);
+  });
+
+const page = async () => {
+  // 2 second delay
+  await delay(2000);
   return (
     <div>
       <h2>this is profile</h2>
